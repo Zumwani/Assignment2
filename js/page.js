99c4082e5db7379bb4d746621e61d1e0c6cfa40a
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2022-10-11 10:38:59 +0200

    Added breadcrumb to contact page.
    - Added breadcrumb to contact page.
    - Made current page get selected in header links.
    - Added .main-layout2.

--- js/page.js
+++ js/page.js
@@ -1,14 +1,19 @@
+const url = window.location.pathname.split("/").pop().replace(".html", "");
+
 load();
 async function load() {
 
     const header = await fetchElement("part/header.html", "nav");
     const footer = await fetchElement("part/footer.html", "footer");
-
+    
     document.body.prepend(header);
     document.body.append(footer);
 
     if (header == null) console.error("Could not load header!");
     if (footer == null) console.error("Could not load footer!");
+
+    if (header != null)
+        setCorrectPageInHeader();
 
 }
 
@@ -18,3 +23,10 @@ async function fetchElement(url, selector) {
     const parsed = new DOMParser().parseFromString(data, 'text/html');
     return parsed.querySelector(selector);
 }
+
+function setCorrectPageInHeader() {
+    document.querySelectorAll("a.main-link").forEach((link) => {
+        link.classList.toggle("active", url == link.getAttribute("href").replace(".html", ""));
+    });
+}
+
